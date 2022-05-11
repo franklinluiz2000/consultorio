a60f017f0e8bd8d59cb7d91b7efb91af44e909f7
frankstark <franklinluiz2000@gmail.com>
2022-05-10 21:35:48 -0300

    consultório concluido

--- medicSearchAdmin/settings/production.py
+++ medicSearchAdmin/settings/production.py
@@ -2,7 +2,7 @@ from .settings import *
 DEBUG = True
 # Crie a secret key para seu ambiente de produção
 SECRET_KEY = 'ixb62hb#ts=ab532u%p1_62-!5w2j==j6d^2-j$!z(@*m+-h'
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://consultorio-frankstark.herokuapp.com/']
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 DATABASES = {
